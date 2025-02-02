b7291a8da2167ead5a9b5558d8f92c25b59158e1
Adrian T <adrian.thorburn@gmail.com>
2025-02-02 16:06:51 +1100

    almost there

--- src/app/components/placeholder.tsx
+++ src/app/components/placeholder.tsx
@@ -1,21 +1,35 @@
 import Image from 'next/image';
 import tomatoes from '../../../public/tomatoes.png';
+import etteLogo from '../../../public/Ette_Logo_Pink.png';
 
 export default function PlaceHolder() {
   return (
-    <div className="h-screen flex flex-col justify-between items-center relative text-primary">
+    <div className="h-screen flex flex-col justify-between items-center relative text-primary p-4">
       {/* Centered Content */}
-      <div className="flex flex-col items-center justify-center flex-grow text-center">
-        <Image src={tomatoes} alt="Placeholder Image" width={200} height={200} />
-        <h1 className="text-8xl font-bold mt-4 italic">ette</h1>
-        <p className="text-lg mt-2 uppercase">Opening Soon</p>
-        <p className="text-lg uppercase">10 Bridge St. Sydney 2000</p>
+      <div className="container mx-auto grid grid-cols-12 gap-4 flex-grow text-center items-center">
+        {/* Main Content (Offset on Large Screens) */}
+        <div className="col-span-12 lg:col-start-5 lg:col-span-4 flex flex-col items-center justify-center gap-6">
+          <div className="w-full h-auto relative flex justify-center items-center">
+            <Image src={tomatoes} alt="Placeholder Image" width={300} height={300} />
+          </div>
+          <div className="w-full h-auto relative flex justify-center items-center">
+            <Image src={etteLogo} alt="ette Logo" width={400} height={200} objectFit="contain" />
+          </div>
+          <div>
+            <p className="text-lg mt-2 uppercase">Opening Soon</p>
+            <p className="text-lg uppercase">10 Bridge St. Sydney 2000</p>
+          </div>
+        </div>
       </div>
       
       {/* Bottom Buttons */}
-      <div className="w-full flex justify-between absolute bottom-4 left-0 px-4 uppercase">
-        <a>Sign up for updates</a>
-        <a className="uppercase">Instagram</a>
+      <div className="w-full grid grid-cols-12 absolute bottom-4 left-0 px-4 uppercase">
+        <div className="col-span-1 lg:col-span-1 col-span-6">
+          <a>Subscribe</a>
+        </div>
+        <div className="lg:col-start-11 lg:col-span-2 col-span-6 text-right">
+          <a className="uppercase">@ettesydney</a>
+        </div>
       </div>
     </div>
   );
