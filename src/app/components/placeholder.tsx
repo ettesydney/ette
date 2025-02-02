bdaa725e8feb2b309be8f44dad64723710748468
Adrian T <adrian.thorburn@gmail.com>
2025-02-02 16:40:31 +1100

    fix image size on mobile

--- src/app/components/placeholder.tsx
+++ src/app/components/placeholder.tsx
@@ -9,9 +9,15 @@ export default function PlaceHolder() {
       <div className="container mx-auto grid grid-cols-12 gap-4 flex-grow text-center items-center lg:pb-12">
         {/* Main Content (Offset on Large Screens) */}
         <div className="col-span-12 lg:col-start-5 lg:col-span-4 flex flex-col items-center justify-center gap-16">
-          <div className="w-full h-auto relative flex justify-center items-center">
-            <Image src={tomatoes} alt="Placeholder Image" width={300} height={300} />
-          </div>
+            <div className="w-full h-auto relative flex justify-center items-center">
+              <Image 
+              src={tomatoes} 
+              alt="Placeholder Image" 
+              width={300} 
+              height={300} 
+              className="object-contain h-72 sm:h-72 md:h-80 lg:h-auto xl:h-auto" 
+              />
+            </div>
           <div className="w-full h-auto relative flex justify-center items-center">
             <Image src={etteLogo} alt="ette Logo" width={500} height={300} objectFit="contain" />
           </div>
