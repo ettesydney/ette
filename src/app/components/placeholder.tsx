81163b2fbe00a187556f879f8866bcdf21717952
Adrian T <adrian.thorburn@gmail.com>
2025-02-02 16:11:05 +1100

    change color

--- src/app/components/placeholder.tsx
+++ src/app/components/placeholder.tsx
@@ -6,14 +6,14 @@ export default function PlaceHolder() {
   return (
     <div className="h-screen flex flex-col justify-between items-center relative text-primary p-4">
       {/* Centered Content */}
-      <div className="container mx-auto grid grid-cols-12 gap-4 flex-grow text-center items-center">
+      <div className="container mx-auto grid grid-cols-12 gap-4 flex-grow text-center items-center lg:pb-12">
         {/* Main Content (Offset on Large Screens) */}
-        <div className="col-span-12 lg:col-start-5 lg:col-span-4 flex flex-col items-center justify-center gap-6">
+        <div className="col-span-12 lg:col-start-5 lg:col-span-4 flex flex-col items-center justify-center gap-16">
           <div className="w-full h-auto relative flex justify-center items-center">
             <Image src={tomatoes} alt="Placeholder Image" width={300} height={300} />
           </div>
           <div className="w-full h-auto relative flex justify-center items-center">
-            <Image src={etteLogo} alt="ette Logo" width={400} height={200} objectFit="contain" />
+            <Image src={etteLogo} alt="ette Logo" width={500} height={300} objectFit="contain" />
           </div>
           <div>
             <p className="text-lg mt-2 uppercase">Opening Soon</p>
