664b8fccc00b008deaa0f6a07764e0c8b9d62b00
Adrian T <adrian.thorburn@gmail.com>
2025-02-06 14:12:48 +1100

    changes again wtf

--- src/app/components/placeholder.tsx
+++ src/app/components/placeholder.tsx
@@ -1,48 +1,40 @@
 import Image from 'next/image';
-import tomatoes from '../../../public/tomatoes.png';
-import image from '../../../public/ETTE_WEB2.jpg';
-import etteLogo from '../../../public/Ette_Logo_Pink.png';
 import etteLogoSyd from '../../../public/Ette_Logo_Sydney_Pink.png';
+import facebook from '../../../public/facebook-svgrepo-com.svg';
+import instagram from '../../../public/instagram-svgrepo-com.svg';
 
 export default function PlaceHolder() {
   return (
-    <div className="h-dvh flex flex-col justify-between items-center relative text-primary p-4">
-      {/* Centered Content */}
-      <div className="mx-auto grid grid-cols-12 gap-4 flex-grow text-center items-center lg:pb-12">
-        {/* Main Content (Offset on Large Screens) */}
-        <div className="col-span-12 lg:col-start-4 lg:col-span-6 flex flex-col items-center justify-center gap-8 lg:gap-16">
-            <div className="w-full h-auto relative flex justify-center items-center">
-              <Image 
-              src={image} 
-              alt="Placeholder Image" 
-              height={380} 
-              width={290} 
-              className="object-contain h-72 sm:h-72 md:h-80 lg:h-auto xl:h-auto" 
-              />
-            </div>
-            <div className="w-full h-auto relative flex justify-center items-center mt-0 lg:mt-8">
-            <Image 
-              src={etteLogoSyd} 
-              alt="ette Logo" 
-              width={500} 
-              height={300} 
-              className="object-contain h-28 sm:h-48 md:h-60 lg:h-auto xl:h-auto" 
-            />
-            </div>
-          <div>
-            <p className="lg:text-lg mb-8 uppercase"> An intimate bar and restaurant nestled in the heart of the city, <br/> with a changing menu guided by quality produce and honest flavours.</p>
-            <p className="lg:text-lg uppercase">Opening Late March</p>            
-          </div>
+    <div className="h-dvh flex flex-col justify-center items-center relative text-primary p-4 text-center">
+      <div className="relative w-full flex flex-col items-center lg:max-w-[600px]">
+        <div className="absolute top-1/2 transform -translate-y-1/2 flex justify-center items-center w-full">
+          <Image 
+            src={etteLogoSyd} 
+            alt="ette Logo" 
+            width={600} 
+            height={400} 
+            className="object-contain h-32 sm:h-48 md:h-60 lg:h-auto xl:h-auto" 
+          />
+        </div>
+        <div className="relative mt-[calc(100%+1rem)] lg:mt-[calc(100%+2rem)] text-center">
+          <p className="lg:text-lg mb-8 uppercase">
+            An intimate bar and restaurant nestled in the heart of the city, with a changing menu guided by quality produce and honest flavours.
+          </p>
+          <p className="lg:text-lg uppercase">Opening Late March</p>
         </div>
       </div>
       
-      {/* Bottom Buttons */}
       <div className="w-full grid grid-cols-12 absolute bottom-4 left-0 px-4 uppercase">
-        <div className="col-span-1 lg:col-span-1 col-span-6">
+        <div className="col-span-1 lg:col-span-1 col-span-6 text-center custom-box-border">
           <a href='https://www.sevenrooms.com/explore/ette/subscription' target='_blank'>Subscribe</a>
         </div>
-        <div className="lg:col-start-11 lg:col-span-2 col-span-6 text-right">
-          <a href='https://www.instagram.com/ette.sydney/#' target='_blank' className="uppercase">@ette.sydney</a>
+        <div className="lg:col-start-11 lg:col-span-2 col-span-6 text-right flex items-center justify-end gap-2">
+          <a href="#">
+            <Image src={facebook} alt="Facebook" width={20} height={20} className="fill-current text-primary" />
+          </a>
+          <a href="https://www.instagram.com/ette.sydney/#" target='_blank'>
+            <Image src={instagram} alt="Instagram" width={20} height={20} className="fill-current text-primary" />
+          </a>
         </div>
       </div>
     </div>
